2f8562bb820adb48a41b81e116ef383f56c7982b
ryan4984 <ryan4984@gmail.com>
2022-02-10 16:34:27 -0600

    added helper script to convert ui files

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ This requires
 - gunicorn
 
  This should all work in either a virtual environment or a conda environment (everything installed with pip). For freezing (packaging) and creating installer files a custom fork of fbs is required (TODO: setup actual fork and link it here).
+
+# Converting UI files
+A helper script `convert_ui_files.py` has been included to automatically convert all \*.ui files to \*.py files. This assumes that pyuic5 is installed and the \*.ui files are in the standard location.
---
+++ convert_ui_files.py
@@ -0,0 +1,36 @@
+"""Automates converting *.ui files from the GUI to *.py files."""
+from os.path import splitext
+from glob import glob
+import ntpath
+import subprocess
+
+
+PROGRAM = 'pyuic5'
+UI_DIR = 'src/main/python/gui/ui'
+
+
+def path_leaf(path):
+    """Properly get the file from an arbitrary path independent of the OS.
+
+    Parameters
+    ----------
+    path : string
+        Full path of a file.
+
+    Returns
+    -------
+    string
+        Name of the file with the file extension.
+
+    """
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+
+if __name__ == "__main__":
+    for file in glob('{:s}/*.ui'.format(UI_DIR)):
+        f_name = splitext(path_leaf(file))[0]
+        args = [PROGRAM, '-x', '{:s}/{:s}.ui'.format(UI_DIR, f_name),
+                '-o', '{:s}/{:s}.py'.format(UI_DIR, f_name)]
+        print(' '.join(args))
+        subprocess.run(args)
