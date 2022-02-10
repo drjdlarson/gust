a9d86d663ba76706af8151869d52f5f1fa3d6242
ryan4984 <ryan4984@gmail.com>
2022-02-10 16:41:28 -0600

    added back api request to gui

--- src/main/python/gui/launcher.py
+++ src/main/python/gui/launcher.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5.QtCore import pyqtSlot
+import requests
 
 from .ui.launcher import Ui_Launcher
 
@@ -18,3 +19,9 @@ class Launcher(QMainWindow, Ui_Launcher):
     @pyqtSlot()
     def client_clicked(self):
         print('clicked client')
+        url = "http://localhost:8000/adder/{}/{}".format(40, 2)
+        r = requests.get(url)
+        json = r.json()
+        answer = json['calc']
+        QMessageBox.question( self, "Message", "Answer: {}".format(str(answer)),
+                             QMessageBox.Ok, QMessageBox.Ok)
