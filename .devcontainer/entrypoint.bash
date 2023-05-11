3446963776b50f32210073d585b75d2e02fce98d
abhandari11 <22saaras@gmail.com>
2023-05-11 11:35:58 -0500

    added dev containers

---
+++ .devcontainer/entrypoint.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+python -m pip install /workspace/repos/fbs
+python -m pip install /workspace/repos/lager_sensors/radio_receiver
+python -m pip install /workspace/repos/lager_sensors/zed
+# echo Hello world. 
+/bin/bash
