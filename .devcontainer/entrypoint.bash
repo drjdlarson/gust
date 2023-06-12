c94bc9f2aadde13be3cfb417a9210dea17f602de
abhandari11 <22saaras@gmail.com>
2023-06-12 14:34:47 -0500

    updating for test

--- .devcontainer/entrypoint.bash
+++ .devcontainer/entrypoint.bash
@@ -4,16 +4,16 @@ echo "running the entrypoint script..."
 mkdir testing_dir
 
 # installing the zed and fbs packages
-pip install gust/repos/fbs/
-pip install gust/repos/lager_sensors/zed/
+pip install /workspaces/gust/repos/fbs/
+pip install /workspaces/gust/repos/lager_sensors/zed/
 
 # installing the packages
-pip install gust/utilities/
-pip install gust/gust_packages/wsgi_apps/
-pip install gust/gust_packages/radio_manager/
+pip install /workspaces/gust/utilities/
+pip install /workspaces/gust/gust_packages/wsgi_apps/
+pip install /workspaces/gust/gust_packages/radio_manager/
 
 # overriding what fbs installs
 pip install pyinstaller==4.9
 
-# exec "$@"
-/bin/bash
+exec "$@"
+# /bin/bash
