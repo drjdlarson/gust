83785c3e376e1734c2544fde045c42d86e0f0a2b
abhandari11 <22saaras@gmail.com>
2023-07-17 13:56:42 -0500

    fixed path in the build scripts, added dockerfile to create deploy image

--- build_scripts/build_radio_manager.sh
+++ build_scripts/build_radio_manager.sh
@@ -33,19 +33,19 @@ done
 
 echo "Building radio manager app ..."
 
-rm -r ./src/main/resources/base/radio_manager
-mkdir ./src/main/resources/base/radio_manager
+rm -r ../src/main/resources/base/radio_manager
+mkdir ../src/main/resources/base/radio_manager
 
 pyinstaller --onefile --noconfirm --noupx --clean \
 	-n radio_manager \
-	-p ./gust_packages/radio_manager \
+	-p ../gust_packages/radio_manager \
 	--hidden-import lxml \
 	--hidden-import lxml.etree \
 	--hidden-import urllib2 \
 	--hidden-import urlparse \
 	--collect-all pymavlink \
 	--collect-all dronekit \
-	--distpath ./src/main/resources/base/radio_manager \
-	--workpath ./gust_packages/radio_manager/build \
-	--specpath ./gust_packages/radio_manager \
-	gust_packages/radio_manager/src/radio_manager/cli.py
+	--distpath ../src/main/resources/base/radio_manager \
+	--workpath ../gust_packages/radio_manager/build \
+	--specpath ../gust_packages/radio_manager \
+	../gust_packages/radio_manager/src/radio_manager/cli.py
